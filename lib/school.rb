e8ef10ca2bef6e9c91c18370b1b64732508ca641
Eric Clark <eclark0013@gmail.com>
2019-06-21 15:21:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,9 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade]=[]
+    if @roster[grade]==nil
+      @roster[grade]=[name]
+    else
+      @roster[grade]<<name
   end
 end
